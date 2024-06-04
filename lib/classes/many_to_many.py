ddf27f9c105c1dbc611e630897a6f44999207a2f
Alistairs <alistairs.gatheru@student.moringaschool.com>
2024-06-04 15:59:45 +0300

    code challenge

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,128 @@
-class Article:
-    def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
-        
 class Author:
     def __init__(self, name):
-        self.name = name
+        if hasattr(self, '_name'):
+            raise AttributeError("Can't change name after it is set")
+        if isinstance(name, str) and len(name) > 0:
+            self._name = name
+        else:
+            raise ValueError("Name must be a non-empty string")
+        self._articles = []
+
+    @property
+    def name(self):
+        return self._name
 
     def articles(self):
-        pass
+        return self._articles
 
     def magazines(self):
-        pass
+        return list(set(article.magazine for article in self._articles))
 
     def add_article(self, magazine, title):
-        pass
+        article = Article(self, magazine, title)
+        self._articles.append(article)
+        magazine._articles.append(article)
+        return article
 
     def topic_areas(self):
-        pass
+        if not self._articles:
+            return None
+        return list(set(article.magazine.category for article in self._articles))
+
 
 class Magazine:
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
+        if isinstance(name, str) and 2 <= len(name) <= 16:
+            self._name = name
+        else:
+            raise ValueError("Name must be a string between 2 and 16 characters")
+        if isinstance(category, str) and len(category) > 0:
+            self._category = category
+        else:
+            raise ValueError("Category must be a non-empty string")
+        self._articles = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 2 <= len(value) <= 16:
+            self._name = value
+        else:
+            raise ValueError("Name must be a string between 2 and 16 characters")
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._category = value
+        else:
+            raise ValueError("Category must be a non-empty string")
 
     def articles(self):
-        pass
+        return self._articles
 
     def contributors(self):
-        pass
+        return list(set(article.author for article in self._articles))
 
     def article_titles(self):
-        pass
+        if not self._articles:
+            return None
+        return [article.title for article in self._articles]
 
     def contributing_authors(self):
-        pass
+        author_counts = {}
+        for article in self._articles:
+            if article.author in author_counts:
+                author_counts[article.author] += 1
+            else:
+                author_counts[article.author] = 1
+        return [author for author, count in author_counts.items() if count > 2] or None
+
+
+class Article:
+    def __init__(self, author, magazine, title):
+        self._set_author(author)
+        self._set_magazine(magazine)
+        self._set_title(title)
+
+    def _set_author(self, author):
+        if hasattr(self, '_author'):
+            raise AttributeError("Can't change author after it is set")
+        if isinstance(author, Author):
+            self._author = author
+        else:
+            raise ValueError("Author must be of type Author")
+        
+    def _set_magazine(self, magazine):
+        if hasattr(self, '_magazine'):
+            raise AttributeError("Can't change magazine after it is set")
+        if isinstance(magazine, Magazine):
+            self._magazine = magazine
+        else:
+            raise ValueError("Magazine must be of type Magazine")
+        
+    def _set_title(self, title):
+        if hasattr(self, '_title'):
+            raise AttributeError("Can't change title after it is set")
+        if isinstance(title, str) and 5 <= len(title) <= 50:
+            self._title = title
+        else:
+            raise ValueError("Title must be a string between 5 and 50 characters")
+    
+    @property
+    def author(self):
+        return self._author
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @property
+    def title(self):
+        return self._title
